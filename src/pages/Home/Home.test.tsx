acaf1221ffdd8b8627a9e260e2518cd128b38e13
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-18 12:06:55 +0100

    Test(🔴): Home has a header

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,4 +1,4 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import { Home } from "./Home";
 import {expect} from "vitest";
 
@@ -9,4 +9,12 @@ describe("Home", () => {
         expect(container).toBeInTheDocument()
         expect(container).not.toBeEmptyDOMElement()
     });
+
+    it("has a header", () => {
+        render(<Home />)
+
+        const header = screen.getByRole("navigation")
+
+        expect(header).toBeInTheDocument()
+    })
 })
