55014e663c550894669d2945a5a522a0d4d32674
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-18 12:23:25 +0100

    Added styles to navbar and title page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import {NavBar} from "@components/NavBar/NavBar.tsx";
 export const Header = () => {
     return (
         <header className="h-14 flex content-center flex-wrap border-b-2 pl-8">
-            <h2>Roberto del Pino</h2>
+            <h2 className="w-[85%]">Roberto del Pino</h2>
             <NavBar />
         </header>
     );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import {HiMenuAlt3} from "react-icons/all";
 
 export const NavBar = () => (
-    <nav>
+    <nav className="flex content-center flex-wrap">
         <HiMenuAlt3 role="graphics-document" className="lg:hidden"></HiMenuAlt3>
     </nav>
 )
