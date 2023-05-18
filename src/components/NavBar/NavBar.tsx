70701bb574427d1207032d64b18867ef42cfd784
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-18 13:28:20 +0100

    Test(🟢): NavBar contains links to other part of portfolio

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,13 @@ import {HiMenuAlt3} from "react-icons/all";
 
 export const NavBar = () => (
     <nav className="flex content-center flex-wrap mx-3">
+        <ul role="list" aria-label="links-pages-container">
+            <li role="link"><a href="#">Inicio</a></li>
+            <li role="link"><a href="#">Sobre mi</a></li>
+            <li role="link"><a href="#">Projects</a></li>
+            <li role="link"><a href="#">Blog</a></li>
+            <li role="link"><a href="#">Contact</a></li>
+        </ul>
         <HiMenuAlt3 role="graphics-document" className="dark:text-white lg:hidden"></HiMenuAlt3>
     </nav>
 )
