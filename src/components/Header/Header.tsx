a4c4a86d43db5b02c3353ae3dc6e1db244caeebd
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-18 12:17:59 +0100

    Added styles to header and navbar components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import {NavBar} from "@components/NavBar/NavBar.tsx";
 
 export const Header = () => {
     return (
-        <header>
+        <header className="h-14 flex content-center flex-wrap border-b-2 pl-8">
             <NavBar />
         </header>
     );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,6 @@ import {HiMenuAlt3} from "react-icons/all";
 
 export const NavBar = () => (
     <nav>
-        <HiMenuAlt3 role="graphics-document"></HiMenuAlt3>
+        <HiMenuAlt3 role="graphics-document" className="lg:hidden"></HiMenuAlt3>
     </nav>
 )
