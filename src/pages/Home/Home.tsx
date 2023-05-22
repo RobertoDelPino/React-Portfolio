21d0b9f26f998b434ea4679f4bd4fddfec135bfd
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-22 12:24:10 +0100

    Added style to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,17 @@ export const Home = () => (
     <>
         <Header />
 
-        <section className="flex w-[100%] justify-center">
+        <hr className="max-w-screen-xl m-auto"/>
+
+        <section className=" max-w-screen-xl m-auto flex w-[100%] justify-around py-32 dark:bg-gray-800">
             <div className="flex content-center flex-wrap">
-                <h2 className="w-96 text-center">Developer</h2>
+                <h1 className="text-4xl dark:text-white w-[100%] ">
+                    Hola! <br/>
+                    <b>Soy Roberto del Pino Hormiga</b> <br/>
+                </h1>
+                <h1 className="text-3xl dark:text-white mt-1.5">Developer</h1>
             </div>
-            <img src={photo} alt="Photo of Roberto del Pino Hormiga" className="w-72 rounded-full"/>
+            <img src={photo} alt="Photo of Roberto del Pino Hormiga" className="w-80 rounded-full"/>
         </section>
     </>
 );
