e5f64f1a02983b5e4a7ba6c2a548adcc6d78f178
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-22 12:10:34 +0100

    Test(🟢): Home has an img as a personal photo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,14 @@
 import {Header} from "@components/Header/Header.tsx";
-
+import photo from "../../assets/fotoPerfil-1.jpg"
 export const Home = () => (
     <>
         <Header />
+
+        <section className="flex w-[100%] justify-center">
+            <div className="flex content-center flex-wrap">
+                <h2 className="w-96 text-center">Developer</h2>
+            </div>
+            <img src={photo} alt="Photo of Roberto del Pino Hormiga" className="w-72 rounded-full"/>
+        </section>
     </>
 );
