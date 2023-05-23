101de2cd4aa66e08bb2f6ad54d55de27e1166cf7
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-23 12:26:19 +0100

    Added personal photo, social media links and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import {Collapse, Navbar} from "@material-tailwind/react";
 import {DarkModeSwitcher} from "@components/DarkModeSwitcher/DarkModeSwitcher.tsx";
 import {useState} from "react";
-import {HiMenuAlt3} from "react-icons/all";
+import { BsInstagram, BsTwitter, GoMail, HiMenuAlt3 } from "react-icons/all";
 
 export const Header = () => {
 
@@ -9,21 +9,28 @@ export const Header = () => {
     const toggleOpen = () => setOpenNav(cur => !cur);
 
     const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6  dark:text-white h-[100%]" role="list" aria-label="links-pages-container">
-            <li className="inline-block mx-2 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Inicio</a></li>
-            <li className="inline-block mx-2 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Sobre mi</a></li>
-            <li className="inline-block mx-2 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Projects</a></li>
-            <li className="inline-block mx-2 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Blog</a></li>
-            <li className="inline-block mx-2 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Contact</a></li>
-        </ul>
+        <>
+            <ul className="justify-end mr-3 mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6  dark:text-white h-[100%]" role="list" aria-label="links-pages-container">
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Inicio</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Sobre mi</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Projects</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Blog</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Contact</a></li>
+                <ul className="flex ">
+                    <li className="text-2xl p-1 mr-3 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600"><a href="#"> <BsInstagram/> </a></li>
+                    <li className="text-2xl p-1 mr-3 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600"><a href="#"> <BsTwitter/> </a></li>
+                    <li className="text-2xl p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600"><a href="#"> <GoMail /> </a></li>
+                </ul>
+            </ul>
+        </>
     )
 
     return (
         <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 dark:bg-gray-800 flex flex-wrap content-center justify-between" >
 
-            <h2 className="inline-block leading-10 dark:text-white text-xl font-bold w-[50%]"><a className="" href="#">roberto.ph</a></h2>
-            <div className="flex">
-                <div className="hidden lg:block">{navList}</div>
+            <h2 className="inline-block leading-10 dark:text-white text-xl font-bold w-[70%] lg:w-[25%]"><a className="" href="#">roberto.ph</a></h2>
+            <div className="flex justify-end w-[30%] lg:w-[75%]">
+                <div className="hidden lg:block w-[90%]">{navList}</div>
                 <DarkModeSwitcher/>
                 <button onClick={toggleOpen} className="lg:hidden">
                     <HiMenuAlt3 role="graphics-document" className="dark:text-white"></HiMenuAlt3>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,15 +6,15 @@ export const Home = () => (
 
         <hr className="max-w-screen-xl m-auto"/>
 
-        <section className=" max-w-screen-xl m-auto flex w-[100%] justify-around py-32 dark:bg-gray-800">
-            <div className="flex content-center flex-wrap">
-                <h1 className="text-4xl dark:text-white w-[100%] ">
+        <section className=" max-w-screen-xl m-auto flex flex-wrap w-[100%] justify-around py-10 lg:py-32 dark:bg-gray-800 ">
+            <div className="flex content-center flex-wrap px-6 mb-9">
+                <h1 className="text-2xl dark:text-white w-[100%] lg:text-4xl">
                     Hola! <br/>
-                    <b>Soy Roberto del Pino Hormiga</b> <br/>
+                    Soy <b>Roberto del Pino Hormiga</b> <br/>
                 </h1>
-                <h1 className="text-3xl dark:text-white mt-1.5">Developer</h1>
+                <h1 className="text-xl dark:text-white mt-1.5 lg:text-2xl">Developer</h1>
             </div>
-            <img src={photo} alt="Photo of Roberto del Pino Hormiga" className="w-80 rounded-full"/>
+            <img src={photo} alt="Photo of Roberto del Pino Hormiga" className="w-72 lg:w-80 rounded-full border-black border-2 dark:border-white"/>
         </section>
     </>
 );
