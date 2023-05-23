b83536333d4b193e54b7886a3ba25a6ebfd4555e
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-23 17:51:31 +0100

    Test(🔴): TechnologiesList show item list

---
+++ src/components/TecnologiesList/TechnologiesList.test.tsx
@@ -0,0 +1,19 @@
+import {render, screen, within} from "@testing-library/react";
+import {TechnologiesList, Technology} from "@components/TecnologiesList/TechnologiesList.tsx";
+import javascript from "../../assets/TechIcons/javascript.png"
+
+describe("TechnologiesList", () => {
+    it("show item list", function () {
+
+        const technologies: Technology[] = [{
+            name: "javascript",
+            image: javascript
+        }]
+
+        const {container} = render( <TechnologiesList  list={technologies}/> )
+
+        const techList = within(container).getAllByRole("article",{name:"technology"})
+
+        expect(techList.length).not.toBe(0)
+    });
+})
