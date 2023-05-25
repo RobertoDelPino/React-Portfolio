cfd05e4b13f513bc87eaa29e22aedc7b252ac4c2
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-25 09:58:41 +0100

    Changed links in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,8 +11,8 @@ export const Header = () => {
     const navList = (
         <>
             <ul className="justify-end mr-3 mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6  dark:text-white h-[100%]" role="list" aria-label="links-pages-container">
-                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Inicio</a></li>
-                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Sobre mi</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="/">Inicio</a></li>
+                <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="/about">Sobre mi</a></li>
                 <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Projects</a></li>
                 <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Blog</a></li>
                 <li className="inline-block mx-1 p-1 transition duration-300 hover:bg-blue-50 dark:hover:bg-gray-600" role="link"><a href="#">Contact</a></li>
@@ -29,7 +29,7 @@ export const Header = () => {
         <>
             <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 dark:bg-gray-800 flex flex-wrap content-center justify-between" >
 
-                <h2 className="inline-block leading-10 dark:text-white text-xl font-bold w-[70%] lg:w-[25%]"><a className="" href="#">roberto.ph</a></h2>
+                <h2 className="inline-block leading-10 dark:text-white text-xl font-bold w-[70%] lg:w-[25%]"><a className="" href="/">roberto.ph</a></h2>
                 <div className="flex justify-end w-[30%] lg:w-[75%]">
                     <div className="hidden lg:block w-[90%]">{navList}</div>
                     <DarkModeSwitcher/>
