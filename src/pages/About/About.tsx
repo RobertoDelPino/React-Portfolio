7813238598a15c42012d12c94e9c5365fd8aa2e4
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-25 10:08:32 +0100

    Test(🟢): About page contains an 'about me' section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,19 @@
+
+{/*
+
+            About me        --> Sobre mi
+            Work experience --> Experiencia de trabajo
+            Education       --> Educación
+
+            */}
+
+
 export const About = () => {
     return (
-        <></>
+        <>
+            <section role="region" aria-label="About me region">
+
+            </section>
+        </>
     )
 }
