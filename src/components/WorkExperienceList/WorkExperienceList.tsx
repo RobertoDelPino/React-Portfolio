16e54ccc27e7031a388a8f9269e10d1629811f5a
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-29 09:55:03 +0100

    Created WorkExperience interface

--- src/components/WorkExperienceList/WorkExperienceList.tsx
+++ src/components/WorkExperienceList/WorkExperienceList.tsx
@@ -1,9 +1,38 @@
 import {WorkExperienceCard} from "@components/WorkExperienceCard/WorkExperienceCard.tsx";
 
+enum workday {
+    FullTime = "Full time",
+    PartTime = "Part time"
+}
+
+interface WorkExperience {
+    name: string
+    company: string
+    urlCompany: string
+    time: string
+    workday: workday,
+    workplace: string
+
+}
+
 export function WorkExperienceList() {
-    return <section className="border-b-2 pb-4 md:w-[70%] my-10">
+    
+    const list: WorkExperience[] = [
+        {
+            name: "Prácticas de Web Developer",
+            company: "LeanMind",
+            urlCompany: "http://www.leanmind.es",
+            time: "Marzo 2023 - Mayo 2023",
+            workplace: "San Cristóbal de La Laguna",
+            workday: workday.FullTime
+        }
+    ]
+    
+    return (
+        <section className="border-b-2 pb-4 md:w-[70%] my-10">
 
-        <WorkExperienceCard/>
+            <WorkExperienceCard/>
 
-    </section>;
+        </section>
+    )
 }
