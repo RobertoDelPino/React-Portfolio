27017212dd0e92a977feab5bf6685f732662d1a0
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-31 21:31:02 +0100

    Created ProjectCard component

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,19 @@
+import {Project} from "@domain/Entities/Project.tsx";
+import {BsGithub, BsLink} from "react-icons/all";
+
+export const ProjectCard = (props: { project: Project }) => <article role="article" aria-label="Project"
+                                                                     className="m-3 w-52 md:w-72 dark:bg-gray-600 rounded-2xl border-2 dark:border-0">
+    <img src={props.project.image} alt={props.project.title}/>
+    <article className="p-4">
+        <h3 className="font-bold text-xl mb-2">{props.project.title}</h3>
+        <p>{props.project.description}</p>
+        <section className="mt-3 md:flex h-10 mb-5 md:mb-0">
+            <p className="mb-2 md:mb-0"><BsLink className="inline h-[100%]"/> <a
+                className="h-[100%] mr-3 text-center underline" target="_blank" href={props.project.previewUrl}>Live
+                Preview</a></p>
+            <p><BsGithub className="inline h-[100%]"/> <a className="h-[100%] mr-4 text-center underline"
+                                                          target="_blank" href={props.project.githubUrl}>Ver
+                código</a></p>
+        </section>
+    </article>
+</article>;
--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,5 +1,5 @@
 import {Project} from "@domain/Entities/Project.tsx";
-import {BsGithub, BsLink} from "react-icons/all";
+import {ProjectCard} from "@components/ProjectCard/ProjectCard.tsx";
 
 interface ProjectsListProps {
     list: Project[]
@@ -9,17 +9,7 @@ export const ProjectsList = ({list}:ProjectsListProps) => (
     <section role="region" aria-label="Project List Section" className="flex justify-center flex-wrap mt-5">
         {
             list.map((project: Project, index) => (
-                <article role="article" aria-label="Project" key={index} className="m-3 w-52 md:w-72 dark:bg-gray-600 rounded-2xl border-2 dark:border-0">
-                    <img src={project.image} alt={project.title}/>
-                    <article className="p-4">
-                        <h3 className="font-bold text-xl mb-2">{project.title}</h3>
-                        <p>{project.description}</p>
-                        <section className="mt-3 md:flex h-10 mb-5 md:mb-0">
-                            <p className="mb-2 md:mb-0"> <BsLink className="inline h-[100%]"/> <a className="h-[100%] mr-3 text-center underline" target="_blank" href={project.previewUrl}>Live Preview</a> </p>
-                            <p> <BsGithub className="inline h-[100%]"/> <a className="h-[100%] mr-4 text-center underline" target="_blank" href={project.githubUrl}>Ver código</a> </p>
-                        </section>
-                    </article>
-                </article>
+                <ProjectCard key={index} project={project}/>
             ))
         }
     </section>
