831c49ce7cb5046c815b4cb77b8eeb2f853c8e43
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-31 15:11:13 +0100

    Added simple info to Project page

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,5 +1,14 @@
-export const ProjectsPage = () => (
-    <section>
+export const ProjectsPage = () => {
 
-    </section>
-);
+    // Maybe use useContext to bring back the projects info?
+
+    return (
+        <section className=" 2xl:min-h-[95%] md:min-h-[80%] dark:bg-gray-800">
+            <article className="max-w-screen-xl m-auto dark:text-white mt-20">
+                <h2 className="text-3xl font-bold text-center">Proyectos</h2>
+                <p className="text-center text-lg">Proyectos que he creado hasta ahora</p>
+
+            </article>
+        </section>
+    );
+}
