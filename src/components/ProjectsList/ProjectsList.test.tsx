1294980671d1ac7166495f30e562616a701e7894
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-31 20:54:43 +0100

    Test(🟢): ProjectsList renders a list of projects

--- src/components/ProjectsList/ProjectsList.test.tsx
+++ src/components/ProjectsList/ProjectsList.test.tsx
@@ -1,8 +1,24 @@
+import {render, screen, within} from "@testing-library/react";
+import {ProjectsList} from "@components/ProjectsList/ProjectsList.tsx";
+import {Project} from "@domain/Entities/Project.tsx";
+
 describe("ProjectsList", () => {
     /*
     *
     * 1. Renders a list of projects
     * 2. Renders an error message when list is empty
     *
-    * */
+    */
+
+    it("renders a list of projects", function () {
+
+        const list: Project[] = [new Project("https://www.github.com", "Portfolio", "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", "https://robertodelpino.netlify.com", "")]
+
+        render(<ProjectsList  list={list}/>)
+
+        const projectListSection = screen.getByRole("region", {name: "Project List Section"})
+        const allProjects = within(projectListSection).getAllByRole("article", {name: "Project"})
+
+        expect(allProjects).not.toBe(null)
+    });
 })
--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,1 +1,26 @@
-export const ProjectsList = () => <section role="region" aria-label="Project List Section"></section>;
+import {Project} from "@domain/Entities/Project.tsx";
+import {BsGithub, BsLink} from "react-icons/all";
+
+interface ProjectsListProps {
+    list: Project[]
+}
+
+export const ProjectsList = ({list}:ProjectsListProps) => (
+    <section role="region" aria-label="Project List Section" className="flex justify-center flex-wrap">
+        {
+            list.map((project: Project, index) => (
+                <article role="article" aria-label="Project" key={index} className="m-3 w-64 md:w-80 bg-gray-600 rounded-2xl">
+                    <img src={project.image} alt={project.title}/>
+                    <article className="p-4">
+                        <h3 className="font-bold text-xl">{project.title}</h3>
+                        <p>{project.description}</p>
+                        <section className="mt-3 md:flex h-10 mb-5 md:mb-0">
+                            <p className="mb-2 md:mb-0"> <BsLink className="inline h-[100%]"/> <a className="h-[100%] mr-3 text-center underline" target="_blank" href={project.previewUrl}>Live Preview</a> </p>
+                            <p> <BsGithub className="inline h-[100%]"/> <a className="h-[100%] mr-4 text-center underline" target="_blank" href={project.githubUrl}>View Code</a> </p>
+                        </section>
+                    </article>
+                </article>
+            ))
+        }
+    </section>
+)
