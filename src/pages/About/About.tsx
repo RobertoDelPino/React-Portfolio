df230ea34ba103a238dfd206b40cb789e8ecf029
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-31 11:25:17 +0100

    Added styles to make it responsive

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -14,7 +14,7 @@ Education       --> Educación
 export const About = () => {
     return (
         <>
-            <section className="dark:bg-gray-800">
+            <section className="dark:bg-gray-800 2xl:min-h-[95%] md:min-h-[80%]">
                 <section className="dark:bg-gray-800 max-w-screen-xl m-auto lg:p-8 p-4">
                     <article role="region" aria-label="About me region" className="my-10">
                         <h2 className="text-2xl font-extrabold tracking-wide dark:text-white my-4">Sobre mi</h2>
