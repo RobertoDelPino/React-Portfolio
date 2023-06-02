556f309b36bdc6348b0e0e6fec8fba0546de4eb7
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-06-02 11:08:11 +0100

    Added actual work experience

--- src/components/WorkExperienceList/WorkExperienceList.tsx
+++ src/components/WorkExperienceList/WorkExperienceList.tsx
@@ -5,6 +5,14 @@ export const WorkExperienceList = () => {
 
     const list: WorkExperience[] = [
         {
+            name: "Web developer",
+            company: "SaludOnNet",
+            urlCompany: "https://www.saludonnet.com/",
+            time: "Junio 2023 - Actualidad",
+            workplace: "San Cristóbal de La Laguna",
+            workday: workday.FullTime
+        },
+        {
             name: "Prácticas de Web Developer",
             company: "LeanMind",
             urlCompany: "https://www.leanmind.es",
