494699db5065245398f7021931fe4338ccd8a187
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-09-21 19:45:40 +0100

    Move blog entry test to component folder and start creating blog page

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -1,80 +1,35 @@
-import remarkGfm from 'remark-gfm'
-import ReactMarkdown from 'react-markdown'
-import prueba from '../../assets/BlogFiles/Prueba.md?raw'
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown';
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-import python from 'highlight.js/lib/languages/python';
-
 export const BlogPage = () => {
-    console.warn = () => console.log();
-    SyntaxHighlighter.registerLanguage('markdown', markdown);
-    setTimeout(() => {
-        hljs.registerLanguage('javascript', javascript);    
-        hljs.registerLanguage('python', python);   
-        hljs.highlightAll();
-    }, 10);
+  const blogFilesNames = [
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"¿Cómo mejorar como desarrollador de aplicaciones?", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"python"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    },
+    {
+      "title":"Nuevos métodos de EMC 6", 
+      "date":"2022-03-23",
+      "description":"Te comparto 5 métodos de javascript para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    }
+  ]
 
-    const components = {
-        a: (a: any) =>{
-          return (
-            <a href={a.href} className='olaaaaa' target="_blank">
-              {a.children}
-            </a>
-          )
-        },
-        p: (p: any) =>{
-            const children = p.children;
-            if(typeof(children[0]) == "string"){
-                if(children[0].includes("title")){
-                    console.log("esto es el titulo")
-                    console.log(JSON.parse(children[0]))
-                }
-            }
 
-            return (
-              <p className='dark:text-white mt-4'>
-                {p.children}
-              </p>
-            )
-        },
-        h1: (h1: any) =>{
-            return (
-              <h1 className='text-2xl dark:text-white mt-12 mb-6 font-bold'>
-                {h1.children}
-              </h1>
-            )
-        }, //margin: 48px 0 24px;
-        h2: (h2: any) =>{
-            return (
-              <h2 className='text-xl dark:text-white mt-10 mb-6 font-bold'>
-                {h2.children}
-              </h2>
-            )
-        },
-        h3: (h2: any) =>{
-            return (
-              <h2 className='text-xl dark:text-white mt-10 mb-6 font-bold'>
-                {h2.children}
-              </h2>
-            )
-        },
 
-      }
-
-    return (
-        <section className=" 2xl:min-h-[95%] xl:min-h-[70%] md:min-h-[80%] dark:bg-gray-800">
-            <article className="max-w-screen-xl m-auto dark:algo mt-7">
-                <ReactMarkdown 
-                    className='markdown-body' 
-                    components={components}
-                    remarkPlugins={[remarkGfm]}
-                    
-                    >
-                    {prueba}
-                </ReactMarkdown>
-            </article>
-        </section>
-    );
+  return 
+    <section>
+      
+    </section>
 }
