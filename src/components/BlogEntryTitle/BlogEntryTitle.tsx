41bc538f7aeb29cdd9ce34abe61871b3d00c90a0
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-09-25 13:34:10 +0100

    Added text when show blog entry creation date

--- src/components/BlogEntryTitle/BlogEntryTitle.tsx
+++ src/components/BlogEntryTitle/BlogEntryTitle.tsx
@@ -7,7 +7,7 @@ export const BlogEntryTitle = (props: { data: BlogEntryData }) => {
       <img className="w-[80px]" src={`../../../src/assets/TechIcons/${props.data.topic}.webp`} alt="" />
       <div className="ml-5">
         <h4 className="dark:text-white text-3xl mb-2.5 font-bold">{props.data.title}</h4>
-        <p className="dark:text-white/70 text-xs">{props.data.date}</p>
+        <p className="dark:text-white/70 text-xs">Fecha de publicación: {props.data.date}</p>
       </div>
     </div>
   )
