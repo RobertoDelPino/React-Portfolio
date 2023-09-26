a0c3ad1f5886576dc51c786510dc5e322cd97cc8
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-09-26 18:55:05 +0100

    adding styles to blog entry title

--- src/components/BlogEntryTitle/BlogEntryTitle.tsx
+++ src/components/BlogEntryTitle/BlogEntryTitle.tsx
@@ -5,11 +5,12 @@ import {BlogEntryData} from "@domain/Entities/BlogEntryData.tsx";
 export const BlogEntryTitle = (props: { data: BlogEntryData }) => {
   const image = findImage(props.data.topic)
   return (
-    <div className="flex mt-5 p-3 rounded">
-      <img className="w-[80px]" src={image} alt="" />
-      <div className="ml-5">
-        <h4 className="dark:text-white text-3xl mb-2.5 font-bold">{props.data.title}</h4>
-        <p className="dark:text-white/70 text-xs">Fecha de publicación: {props.data.date}</p>
+    <div className="flex mt-5 p-3 border-b-[1.8px]">
+      <img className="absolute md:relative z-0 blur-lg md:blur-none w-[80px] rounded" src={image} alt="" />
+      <div className="relative z-1 ml-5 bg-transparent">
+        <h4 className="dark:text-white z-1 md:z-0 text-3xl mb-2.5 font-bold">{props.data.title}</h4>
+        <p className="text-white md:dark:text-white/70 text-xs">Fecha de publicación: {props.data.date}</p>
+        <p className="md:hidden text-white md:dark:text-white/70 text-xs capitalize">{props.data.topic}</p>
       </div>
     </div>
   )
