27fc838e46fc35e152d31e9a1ace2dde4bb20f8a
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-09-30 09:45:05 +0100

    Created articles repository

---
+++ src/domain/Repository/Articles.tsx
@@ -0,0 +1,96 @@
+import { BlogEntryData } from "../Entities/BlogEntryData";
+
+const blogEntriesList : BlogEntryData[] = [
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"Maneras de mejorar como desarrollador de aplicaciones", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"javascript"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    },
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"Maneras de mejorar como desarrollador de aplicaciones", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"javascript"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    },
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"Maneras de mejorar como desarrollador de aplicaciones", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"javascript"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    },
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"Maneras de mejorar como desarrollador de aplicaciones", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"javascript"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    },
+    {
+      "title":"5 consejos para desarrolladores Junior", 
+      "date":"2023-03-23",
+      "description":"Te comparto 5 consejos para desarrolladores que están comenzando en el mundo de la programación",
+      "topic":"javascript"
+    },
+    {
+      "title":"Maneras de mejorar como desarrollador de aplicaciones", 
+      "date":"2023-02-10",
+      "description":"Ideas de proyectos para mejorar como desarollador",
+      "topic":"javascript"
+    },
+    {
+      "title":"Empezar bien el año siendo programador", 
+      "date":"2023-01-01",
+      "description":"Te muestro distintas ideas creativas para empezar con mucha motivación este 2023",
+      "topic":"javascript"
+    }
+]
+
+export default blogEntriesList;
