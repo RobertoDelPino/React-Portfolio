5362de76d88d2a68603a2e18dac68f0ed10bd6d0
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-10-03 07:58:24 +0100

    add tooltip tailwind styles

--- src/components/Technology/Technology.tsx
+++ src/components/Technology/Technology.tsx
@@ -4,6 +4,10 @@ export const Technology = (props: { tech: ITechnology }) => (
     <article data-tooltip={props.tech.name} aria-label="technology" 
         className="mx-4 lg:mx-12 my-4 before:opacity-0 before:content-[attr(data-tooltip)] before:absolute before:text-center
         before:w-24 hover:before:opacity-100 before:mt-[-50px]
+        dark:hover:before:bg-slate-600 dark:text-white
+        before:font-bold hover:before:bg-gray-300 before:p-[10px]
+        before:rounded-lg before:shadow-xl before:transition
+        before:duration-150 before:ease-linear
         "
     >
         <strong>
