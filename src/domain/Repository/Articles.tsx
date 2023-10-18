aba27e03b460bd470cd7881ecd5daab10013dbbe
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-10-18 21:47:02 +0100

    reorder articles

--- src/domain/Repository/Articles.tsx
+++ src/domain/Repository/Articles.tsx
@@ -1,18 +1,18 @@
 import { BlogEntryData } from "../Entities/BlogEntryData";
 
 const blogEntriesList : BlogEntryData[] = [
-    {
-      "title":"Descubriendo el mundo real de la Programacion Mi Primer Trabajo", 
-      "date":"02-10-2023",
-      "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
-      "topic":"javascript"
-    },
-    {
-      "title":"Optimización de queries en SQL para mejorar el rendimiento de la base de datos", 
-      "date":"14-10-2023",
-      "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
-      "topic":"sql"
-    }
+  {
+    "title":"Optimización de queries en SQL para mejorar el rendimiento de la base de datos", 
+    "date":"14-10-2023",
+    "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
+    "topic":"sql"
+  }
+  ,{
+    "title":"Descubriendo el mundo real de la Programacion Mi Primer Trabajo", 
+    "date":"02-10-2023",
+    "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
+    "topic":"javascript"
+  }
 ]
 
 export default blogEntriesList;
