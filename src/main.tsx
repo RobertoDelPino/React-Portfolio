d3ab1a2fbe8079650ef36448de17dc68decab26d
RobertoDPH <robertodelpino11@gmail.com>
2023-12-10 12:39:00 +0000

    fix background color problem

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const rootElement = document.getElementById('root') as HTMLElement;
-rootElement.classList.add('dark:bg-gray-800');
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
-);
+)
+
+const body = document.querySelector('body');
+body.classList.add('dark:bg-gray-800');
 
 console.warn = () => console.log();
