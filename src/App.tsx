bcac93b09cc5045cbc64589a1c734e2fcf0316a2
RobertoDPH <robertodelpino11@gmail.com>
2024-01-06 18:20:38 +0000

    improve code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import {Home} from "@pages/Home/Home";
+import { Footer } from '@components/Footer/Footer';
+import { Header } from '@components/Header/Header';
+import { About } from '@pages/About/About';
+import { BlogEntry } from '@pages/BlogEntry/BlogEntry';
+import { BlogPage } from '@pages/BlogPage/BlogPage';
+import { Home } from '@pages/Home/Home';
+import { NotFound } from '@pages/NotFound/Home';
+import { ProjectsPage } from '@pages/ProjectsPage/ProjectsPage';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Header} from "@components/Header/Header.tsx";
-import {About} from "@pages/About/About.tsx";
-import {Footer} from "@components/Footer/Footer.tsx";
-import {ProjectsPage} from "@pages/ProjectsPage/ProjectsPage.tsx";
-import { BlogPage } from "@pages/BlogPage/BlogPage";
-import { BlogEntry } from "@pages/BlogEntry/BlogEntry";
-import { NotFound } from "@pages/NotFound/Home";
 
-const App = () => (
-    <div className="dark:bg-gray-800 min-h-full max-w-screen-2xl flex flex-col mx-auto">
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={ <Home />}/>
-                <Route path="/about" element={ <About />}/>
-                <Route path="/projects" element={ <ProjectsPage />}/>
-                <Route path="/blog" element={ <BlogPage/> }/>
-                <Route path="/blog/:fileName" element={ <BlogEntry/> } />
-                <Route path="/*" element={ <NotFound/> }/>
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    </div>
-);
+function ScrollToTop() : null {
+  const { pathname, hash } = useLocation();
 
-export default App
+  useEffect(() => {
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    } else {
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+        <ScrollToTop /> 
+        <Header />
+        <Routes>
+            <Route path="/" element={ <Home />}/>
+            <Route path="/about" element={ <About />}/>
+            <Route path="/projects" element={ <ProjectsPage />}/>
+            <Route path="/blog" element={ <BlogPage/> }/>
+            <Route path="/blog/:fileName" element={ 
+                <>
+                    <BlogEntry/>
+                </>
+             } />
+            <Route path="/*" element={ <NotFound/> }/>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
