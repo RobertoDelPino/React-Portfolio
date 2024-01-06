e6e39cb5ab691ee328b69f55996fc9e536ec6712
RobertoDPH <robertodelpino11@gmail.com>
2024-01-06 18:33:02 +0000

    Revert "reset changes"
    This reverts commit 8363d55723296c77ea9ee86a720c9df15a7000ed.

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,45 @@ import { BlogPage } from '@pages/BlogPage/BlogPage';
 import { Home } from '@pages/Home/Home';
 import { NotFound } from '@pages/NotFound/Home';
 import { ProjectsPage } from '@pages/ProjectsPage/ProjectsPage';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function ScrollToTop() : null {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    } else {
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+        <ScrollToTop /> 
+        <Header />
         <Routes>
             <Route path="/" element={ <Home />}/>
             <Route path="/about" element={ <About />}/>
             <Route path="/projects" element={ <ProjectsPage />}/>
             <Route path="/blog" element={ <BlogPage/> }/>
-            <Route path="/blog/:fileName" element={ <BlogEntry/> } />
+            <Route path="/blog/:fileName" element={ 
+                <>
+                    <BlogEntry/>
+                </>
+             } />
             <Route path="/*" element={ <NotFound/> }/>
         </Routes>
         <Footer />
