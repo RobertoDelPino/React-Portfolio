2ec493dcce3288858156178ead83aaed88746c54
RobertoDPH <robertodelpino11@gmail.com>
2024-01-16 12:24:51 +0000

    rename topic of new post

--- src/domain/Repository/Articles.tsx
+++ src/domain/Repository/Articles.tsx
@@ -17,7 +17,7 @@ const blogEntriesList : BlogEntryData[] = [
     "title":"Descubriendo el mundo real de la Programacion Mi Primer Trabajo", 
     "date":"02-10-2023",
     "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
-    "topic":"javascript"
+    "topic":"tddlogo"
   }
 ]
 
