295a2e90f10683bab5d88447935e66278d0c9c66
RobertoDPH <robertodelpino11@gmail.com>
2024-01-16 12:24:00 +0000

    fix order of articles

--- src/domain/Repository/Articles.tsx
+++ src/domain/Repository/Articles.tsx
@@ -2,6 +2,12 @@ import { BlogEntryData } from "../Entities/BlogEntryData";
 
 const blogEntriesList : BlogEntryData[] = [
   {
+    "title":"Introducción a TDD (Desarrollo Dirigido por Pruebas)", 
+    "date":"15-01-2024",
+    "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
+    "topic":"Javascript"
+  },
+  {
     "title":"Optimización de queries en SQL para mejorar el rendimiento de la base de datos", 
     "date":"14-10-2023",
     "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
@@ -12,12 +18,6 @@ const blogEntriesList : BlogEntryData[] = [
     "date":"02-10-2023",
     "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
     "topic":"javascript"
-  }
-  ,{
-    "title":"Introducción a TDD (Desarrollo Dirigido por Pruebas)", 
-    "date":"15-01-2024",
-    "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
-    "topic":"Javascript"
   }
 ]
 
