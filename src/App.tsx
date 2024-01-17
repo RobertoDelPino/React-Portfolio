c4db6227aa46e15d60d6946f65fe6fc1ac369e35
RobertoDPH <robertodelpino11@gmail.com>
2024-01-17 07:22:54 +0000

    move to top when change page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Home} from "@pages/Home/Home";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
 import {Header} from "@components/Header/Header.tsx";
 import {About} from "@pages/About/About.tsx";
 import {Footer} from "@components/Footer/Footer.tsx";
@@ -7,10 +7,21 @@ import {ProjectsPage} from "@pages/ProjectsPage/ProjectsPage.tsx";
 import { BlogPage } from "@pages/BlogPage/BlogPage";
 import { BlogEntry } from "@pages/BlogEntry/BlogEntry";
 import { NotFound } from "@pages/NotFound/Home";
+import { useEffect } from "react";
 
+function ScrollToTop() : null {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]); // escucha los cambios en pathname
+
+    return null;
+}
 const App = () => (
     <div className="dark:bg-gray-800 min-h-full max-w-screen-2xl flex flex-col mx-auto">
         <BrowserRouter>
+            <ScrollToTop />
             <Header />
             <Routes>
                 <Route path="/" element={ <Home />}/>
