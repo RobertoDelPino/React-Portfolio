1341317f3efbb909e9e1deb379604d036bf4e8ef
RobertoDPH <robertodelpino11@gmail.com>
2024-01-17 06:50:10 +0000

    make blog entry card height equal

--- src/components/BlogEntryCard/BlogEntryCard.tsx
+++ src/components/BlogEntryCard/BlogEntryCard.tsx
@@ -7,9 +7,9 @@ export const BlogEntryCard = (props: { blogEntry: BlogEntryData }) => {
   const image = findImage(props.blogEntry.topic);
 
   return (
-    <article className="max-w-xl xl:w-[576px] w-full">
-      <Link to={"/blog/" + filePath}>
-        <div className="flex items-center mb-0.5 transition duration-300 hover:bg-gray-800/[.1] dark:hover:bg-gray-700 p-3 rounded group">
+    <article className="max-w-xl xl:w-[576px] w-full min-h-[100px]">
+      <Link to={"/blog/" + filePath} className="h-full">
+        <div className="h-full flex items-center mb-0.5 transition duration-300 hover:bg-gray-800/[.1] dark:hover:bg-gray-700 p-3 rounded group">
           <img className="w-[50px] h-[50px] group-hover:scale-[1.2] transition duration-500 opacity-80" src={image} alt="" />
           <div className="ml-5">
             <p className="dark:text-white text-lg">{props.blogEntry.title}</p>
