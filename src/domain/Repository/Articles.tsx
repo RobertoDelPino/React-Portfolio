30711011c2fa9981c92d126106ac10a369caaa8f
RobertoDPH <robertodelpino11@gmail.com>
2024-02-03 09:05:10 +0000

    hide first article

--- src/domain/Repository/Articles.tsx
+++ src/domain/Repository/Articles.tsx
@@ -19,12 +19,6 @@ const blogEntriesList : BlogEntryData[] = [
     "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
     "topic":"sql"
   }
-  ,{
-    "title":"Descubriendo el mundo real de la Programacion Mi Primer Trabajo", 
-    "date":"02-10-2023",
-    "description":"Te muestro qué he aprendido durante los primeros meses del trabajo",
-    "topic":"javascript"
-  }
 ]
 
 export default blogEntriesList;
