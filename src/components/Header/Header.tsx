5f55028f734aa9fa7409d05c27d832e7ef885e7d
RobertoDPH <robertodelpino11@gmail.com>
2024-02-19 17:23:12 +0000

    improve header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -102,8 +102,8 @@ export const Header = () => {
     return (
         <>
              <Navbar className={`border-0 text-current mx-auto max-w-screen-xl min-h-[64px] py-0
-                ${isSticky ? "sticky top-0 z-50 dark:bg-gray-900 rounded-b-md rounded-t-none bg-gray-200" : " bg-white rounded-md dark:bg-gray-800"} 
-                transition-colors duration-500 border-b-2 border-gray-300 dark:border-gray-700 w-full px-0
+                ${isSticky ? "sticky top-0 z-50 dark:bg-gray-700/80 bg-gray-200" : " bg-white dark:bg-gray-800/80"} 
+                transition-colors duration-500 border-b-2 border-gray-300 dark:border-gray-700 w-full px-0 shadow-lg backdrop-saturate-200 backdrop-blur-2xl
             `} >
 
                 <section className="py-3 w-full flex flex-wrap content-center justify-around">
