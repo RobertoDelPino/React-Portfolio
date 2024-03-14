168f301277a9f68fd7b1a2f330257f92ee748cda
RobertoDPH <robertodelpino11@gmail.com>
2024-03-14 11:16:44 +0000

    set dark and light colors

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -9,7 +9,7 @@ const Technologies = ( props: TechnologyProps) => {
     const { techList, name } = props;
     return (
         <article id="backend" className="w-full lg:w-[27%] mx-3 lg:mx-0 text-center relative rounded-2xl 
-            p-6 bg-slate-700 shadow-2xl overflow-hidden my-3"
+            p-6 dark:bg-slate-700 bg-slate-200 shadow-2xl overflow-hidden my-3"
         >
             <h3 className="text-2xl font-semibold mb-7">{name}</h3> {/* Change color to title */}
             <section className="flex flex-wrap justify-center flex-row gap-4">
