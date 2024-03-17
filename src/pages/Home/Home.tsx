de1c664fdc4b64d011f7572168c837612f79fb93
RobertoDPH <robertodelpino11@gmail.com>
2024-03-17 20:04:13 +0000

    improve design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -52,7 +52,7 @@ export const Home = () => {
                     
                     <section className="flex flex-wrap justify-center items-center gap-3 mx-3">
                         <article className="w-full flex justify-center flex-wrap m-auto mt-10 p-4 bg-slate-200 rounded-2xl lg:w-8/12 dark:bg-slate-700">
-                            <div className="lg:w-[35%] m-0 lg:mr-2">
+                            <div className="lg:w-[39%] m-0 lg:mr-2">
                                 <h2 className="text-2xl font-bold dark:text-title-dark text-title">Real Time Chat</h2>
                                 <p className="mt-1 font-semibold">30-01-24</p>
                                 <p className="mt-4 font-semibold">Aplicación de chat en tiempo real creada con React, Node.js, Express, MongoDB y Socket.io</p>
