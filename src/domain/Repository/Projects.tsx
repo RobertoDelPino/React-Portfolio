942e51cc623951304f4f5fb61469a6e244b6720d
RobertoDPH <robertodelpino11@gmail.com>
2024-05-08 09:39:46 +0100

    remove projects

--- src/domain/Repository/Projects.tsx
+++ src/domain/Repository/Projects.tsx
@@ -8,20 +8,6 @@ const Projects : Project[] = [
         "http://chat-prueba.online",
         "/ProjectsPhotos/realTimeChat.webp",
         "/projects/realtimechat"),
-    new Project(
-        "https://github.com/RobertoDelPino/React-Portfolio",
-        "Mi portfolio",
-        "Te presento el código de esta página, mi portfolio. Si deseas enviarme cualquier feedback contacta conmigo a través de mi correo.",
-        "https://robertodelpino.dev",
-        "/ProjectsPhotos/RobertoProject.webp",
-        "/projects"),
-    new Project(
-        "https://github.com/RobertoDelPino/Javascript-Course-Projects",
-        "Proyetos de aprendizaje de Javascript",
-        "Aqui podrás ver todos los proyectos en los que he estado trabajando para poder aprender y mejorar en Javascript",
-        "https://github.com/RobertoDelPino/Javascript-Course-Projects",
-        "/ProjectsPhotos/javascriptProjects.webp",
-        "/projects")
 ]
 
 export default Projects;
