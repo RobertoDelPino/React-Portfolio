e31c24612032675481c7638c0e2c815caf325869
RobertoDPH <robertodelpino11@gmail.com>
2024-05-11 22:32:10 +0100

    social links in footer make it inline

--- src/infrastructure/components/Footer/Footer.tsx
+++ src/infrastructure/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => (
             <hr className="" />
             <section className="dark:bg-gray-800 m-auto md:flex md:justify-between dark:text-white px-16 py-3 pb-6">
                 <h2 className="text-center block font-bold">Roberto del Pino Hormiga</h2>
-                <section className="md:flex">
+                <section className="flex">
                     <p className="text-2xl md:px-4 m-auto my-3 text-center"><a aria-label="Link hacia mi LinkedIn page" className="w-3 block m-auto " href="https://www.linkedin.com/in/robertodelpinohormiga/" target="_blank"><BsLinkedin/></a></p>
                     <p className="text-2xl md:px-4 m-auto my-3 text-center"><a aria-label="Link hacia mi Github profile" className="w-3 block m-auto " href="https://github.com/RobertoDelPino" target="_blank"><BsGithub/></a></p>
                     <p className="text-2xl md:px-4 m-auto my-3 text-center"><a aria-label="¿Deseas enviarme un correo electrónico? Pulsa aquí" className="w-3 block m-auto " href="mailto:robertodelpino11@gmail.com" target="_blank"><GoMail/></a></p>
