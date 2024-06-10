10fbc8256549c5b358f5a15fb9085ea24c177f34
RobertoDPH <robertodelpino11@gmail.com>
2024-06-10 19:54:09 +0100

    improve education card design

--- src/infrastructure/pages/About/components/EducationCard/EducationCard.tsx
+++ src/infrastructure/pages/About/components/EducationCard/EducationCard.tsx
@@ -10,24 +10,29 @@ export const EducationCard = ({education}: EducationCardProps) => <section>
             <h3 className="text-xl font-medium dark:text-white">{education.name}</h3>
             <p className="dark:text-gray-300 text-sm leading-7">📅{education.time}</p>
         </article>
-        <p className="mt-3 dark:text-gray-300">🔠
-            <a href={education.instituteUrl} 
-                target="_blank" 
-                className="hover:border-b-2 py-1 hover:text-gray-800 dark:hover:text-gray-200">
-                    {education.institute}
-            </a>
-            {education.place && <span> en {education.place}</span>} 
-        </p>
-        {education.certicateUrl && <a href={education.certicateUrl} target="_blank" className="w-28 block"><p className="p-1 rounded-xl mt-4 bg-slate-300 hover:bg-slate-400 dark:bg-slate-400 dark:hover:bg-slate-300 w-28 text-center text-sm font-semibold ">Ver certificado</p></a>}
+        <article className="md:flex justify-between">
+            <p className="mt-3 dark:text-gray-300">🔠
+                <a href={education.instituteUrl} 
+                    target="_blank" 
+                    className="hover:border-b-2 py-1 hover:text-gray-800 dark:hover:text-gray-200 ">
+                        {education.institute}
+                </a>
+                {education.place && <span> en {education.place}</span>} 
+            </p>
+            {education.certicateUrl && <a href={education.certicateUrl} target="_blank" className="w-28 block"><p className="p-1 rounded-xl mt-3 bg-slate-300 hover:bg-slate-400 dark:bg-slate-400 dark:hover:bg-slate-300 w-28 text-center text-sm font-semibold ">Ver certificado</p></a>}
+        </article>
     </article>
 
     <article className="block md:hidden border-b-2 border-black dark:border-white pb-4 my-8">
-        <h3 className="text-lg font-medium dark:text-white">{education.name}</h3>
+        <article className="flex justify-between">
+            <h3 className="text-lg font-medium dark:text-white">{education.name}</h3>
+            {education.certicateUrl && <a href={education.certicateUrl} target="_blank" className="w-28 block"><p className="p-1 rounded-xl bg-slate-300 hover:bg-slate-400 dark:bg-slate-400 dark:hover:bg-slate-300 w-28 text-center text-sm font-semibold ">Ver certificado</p></a>}
+        </article>
+        
         <p className="mt-4">🔠<a href={education.instituteUrl} target="_blank"
                                className="hover:border-b-2 py-1 text-gray-500 hover:text-gray-600 dark:text-gray-300">{education.institute}</a>
         </p>
         {education.place && <p className="text-gray-500 dark:text-gray-300 my-3">🗺️{education.place}</p> }
         <p className="text-gray-500 dark:text-gray-300 leading-7">📅{education.time}</p>
-        {education.certicateUrl && <a href={education.certicateUrl} target="_blank" className="w-28 block"><p className="p-1 rounded-xl mt-4 bg-slate-300 hover:bg-slate-400 dark:bg-slate-400 dark:hover:bg-slate-300 w-28 text-center text-sm font-semibold ">Ver certificado</p></a>}
     </article>
 </section>
