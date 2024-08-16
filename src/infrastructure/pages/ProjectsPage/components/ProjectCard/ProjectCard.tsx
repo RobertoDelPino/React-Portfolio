d3a64d1ae55dd406f25629e4e258be5e67288c17
RobertoDPH <robertodelpino11@gmail.com>
2024-08-16 15:37:45 +0100

    add aspect ratio to project image

--- src/infrastructure/pages/ProjectsPage/components/ProjectCard/ProjectCard.tsx
+++ src/infrastructure/pages/ProjectsPage/components/ProjectCard/ProjectCard.tsx
@@ -6,7 +6,7 @@ import {BsGithub} from "react-icons/bs";
 export const ProjectCard = (props: { project: Project }) => 
     <article role="article" aria-label="Project" className="m-3 max-w-4xl min-h-[300px] dark:bg-slate-700 rounded-2xl border dark:border-0 flex flex-wrap md:flex-nowrap">
         <div className="md:max-w-md object-cover rounded-t-2xl md:rounded-r-none md:rounded-l-2xl flex items-center justify-center bg-slate-200 dark:bg-cyan-950/20">
-            <img loading="lazy" className="w-11/12" src={props.project.image} alt={props.project.title}/>
+            <img className="aspect-auto w-11/12" src={props.project.image} alt={props.project.title}/>
         </div>
         <article className="p-4">
             <h3 className="font-bold text-xl mb-2">{props.project.title}</h3>
