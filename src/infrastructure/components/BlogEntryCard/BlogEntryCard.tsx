45ab02759a40355e478c074dd5242ceebfccc870
RobertoDPH <robertodelpino11@gmail.com>
2024-10-31 17:12:34 +0000

    remove time tag

--- src/infrastructure/components/BlogEntryCard/BlogEntryCard.tsx
+++ src/infrastructure/components/BlogEntryCard/BlogEntryCard.tsx
@@ -9,7 +9,7 @@ export const BlogEntryCard = (props: { blogEntry: BlogEntryData }) => {
       <img className="w-[50px] h-[50px] group-hover:scale-[1.2] transition duration-500 opacity-80" src={"/TechIcons/" + props.blogEntry.topic + ".svg"} alt="" />
       <div className="ml-5 text-left">
         <p className="dark:text-white text-lg">{props.blogEntry.title}</p>
-        <time dateTime={props.blogEntry.date} className="dark:text-white text-sm">{props.blogEntry.date}</time>
+        <p className="dark:text-white text-sm">{props.blogEntry.date}</p>
       </div>
     </div>
   );
